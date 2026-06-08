6357bf7efa04f2a418b6c66d612aac707bd8d8de
spikereloaded <lewis.198804@gmail.com>
2026-06-08 23:39:05 +0100

    added skills ticker

---
+++ assets/javascript/home.js
@@ -0,0 +1,8 @@
+// const skills = document.querySelectorAll(".skill");
+
+// skills.forEach(skill => {
+//     console.log("added");
+//     skill.addEventListener("click", () =>{
+//         skill.style.display = "none";
+//     })
+// });

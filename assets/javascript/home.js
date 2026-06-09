16b229ac6c07b443ec6196c51e898e0972ee7cfe
spikereloaded <lewis.198804@gmail.com>
2026-06-09 23:40:07 +0100

    Started work on gamification. added score system. Added point notification bubble. Added visual score tracker.

--- assets/javascript/home.js
+++ assets/javascript/home.js
@@ -1,8 +1,99 @@
-// const skills = document.querySelectorAll(".skill");
+const skills = document.querySelectorAll(".skill");
+const random = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+const skillsParent = document.querySelector("#skill-inner")
+const skillContainer = document.querySelector("#skill-wrapper")
+const totalSkills = skillsParent.childElementCount;
+const currentScore = document.querySelector("#current-score");
+const pointBubble = document.querySelector("#point-bubble");
+const mainPortrait = document.querySelector("#main-portrait");
+const thumbnail = document.querySelector("#thumbnail-portrait");
+thumbnail.addEventListener("click", countClicks);
 
-// skills.forEach(skill => {
-//     console.log("added");
-//     skill.addEventListener("click", () =>{
-//         skill.style.display = "none";
-//     })
-// });
+let shot = 0;
+let clicks = 0;
+let pictureClicks = false;
+let score = 0;
+
+skills.forEach(skill => {
+    skill.addEventListener("click", () => {
+        //clone the element that was clicked and set the original to be hidden
+        let clone = skill.cloneNode(true);
+
+        //get the data-skill-name attribute to identify the specific clicked skill
+        //then find duplicate skills and set their visibility to hidden to 
+        //maintain a consitent animation with no jumps/stutters. 
+        let skillName = skill.getAttribute("data-skill-name");
+        let sameSkills = document.querySelectorAll("[data-skill-name=" + skillName + "]");
+        sameSkills.forEach(skillPill => {
+            skillPill.style.visibility = "hidden";
+        });
+
+        //determine the rotation angle, jump height and placement of cloned skill
+        const angle = Math.floor(Math.random() * 721) - 360;
+        let container = skill.parentNode;
+        let containerTop = container.getBoundingClientRect().y;
+        const height = random(10, (containerTop - 100));
+        let leftPos = skill.getBoundingClientRect().x + "px";
+        let topPos = skill.getBoundingClientRect().y + "px";
+
+
+        //set position and styling of cloned skill
+        clone.style.left = leftPos;
+        clone.style.top = topPos;
+        clone.classList.add("shoot-off");
+        clone.style.setProperty("--shotHeight", `${height}px`);
+        clone.style.setProperty("--rotAngle", `${angle}deg`);
+
+        document.body.appendChild(clone);
+        clone.addEventListener("animationend", () => clone.remove());
+
+        let center = skill.getBoundingClientRect().x + (skill.offsetWidth / 2);
+        spawnBubble(center, skill.getBoundingClientRect().y, 10)
+
+        shot += 1;
+        checkEmpty();
+
+    })
+});
+
+function checkEmpty() {
+
+    if ((shot * 2) == totalSkills) {
+        const rect = skillContainer.getBoundingClientRect();
+        let xPosition = rect.left + rect.width / 2;
+        let yPosition = skillContainer.getBoundingClientRect().y;
+        spawnBubble(xPosition, yPosition, 50);
+
+    }
+}
+
+function updateScore(points) {
+    score += points;
+    currentScore.innerText = score;
+}
+//spawns a point bubble at the coordinates passed and points value
+//adds points to overall score
+function spawnBubble(xPos, yPos, points) {
+    let pointClone = pointBubble.cloneNode(true);
+    console.log(xPos, yPos);
+    pointClone.style.left = xPos + "px";
+    pointClone.style.top = yPos + "px";
+    pointClone.innerText = "+" + points;
+
+    pointClone.classList.add("new-points");
+    pointClone.addEventListener("animationend", () => pointClone.remove());
+    document.body.appendChild(pointClone);
+    updateScore(points);
+}
+function countClicks() {
+    clicks += 1;
+    console.log(clicks);
+    if (clicks >= 20 && !pictureClicks) {
+        const rect = thumbnail.getBoundingClientRect();
+        let xPosition = rect.left + rect.width / 2;
+        let yPosition = thumbnail.getBoundingClientRect().y;
+        spawnBubble(xPosition,yPosition,50);
+        pictureClicks = true;
+        document.querySelector("#second-portrait").classList.add("revealed");
+    }
+}
